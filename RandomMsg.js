517f2b519295ba68f47e4575a2d07e2368c1fca0
Christian Ramos <christian.ramos24@upr.edu>
2023-06-13 14:32:56 -0400

    Added functionality to object.

--- RandomMsg.js
+++ RandomMsg.js
@@ -4,4 +4,22 @@ const message = {
     adjective : ['adorable', 'adventurous', 'alive', 'attractive', 'beautiful', 'brave','calm', 
 'charming', 'clean', 'clear', 'clever', 'cloudy', 'colorful', 'combative', 'cooperative', 'courageous'],
 
+    getMessage(){
+
+        let subj = this.subject[Math.floor(Math.random())];
+
+        let ver = '';
+
+        if(subj === 'You'){
+            ver = this.verb[Math.floor(Math.random()*2)];
+        }else{
+            ver = this.verb[(Math.floor(Math.random()*2) + 1) % 3];
+        }
+        
+        let adj = this.adjective[Math.floor(Math.random()*16)];
+        
+        return `${subj} ${ver} ${adj}`
+    }
 };
+
+console.log(message.getMessage())
