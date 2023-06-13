06ea4a47301968d963b6d3886062a3f371d95b33
Christian Ramos <christian.ramos24@upr.edu>
2023-06-13 14:24:33 -0400

    Created an object containing 3 arrays.

--- RandomMsg.js
+++ RandomMsg.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const message = {
+    subject : ['You', 'I'],
+    verb : ['are', 'feel', 'look', 'am'],
+    adjective : ['adorable', 'adventurous', 'alive', 'attractive', 'beautiful', 'brave','calm', 
+'charming', 'clean', 'clear', 'clever', 'cloudy', 'colorful', 'combative', 'cooperative', 'courageous'],
+
+};
