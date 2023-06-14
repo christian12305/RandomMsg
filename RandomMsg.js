96709d1c6901a05f421a10fc103a125c226d6b62
Christian Ramos <christian.ramos24@upr.edu>
2023-06-13 23:29:14 -0400

    Added the message function.

--- RandomMsg.js
+++ RandomMsg.js
@@ -1,23 +1,28 @@
-const message = {
-    _subjec : ['You', 'I'],
+const str = {
+
+    _subject : ['You', 'I'],
     _verb : ['are', 'feel', 'look', 'am'],
     _adjective : ['adorable', 'adventurous', 'alive', 'attractive', 'beautiful', 'brave','calm', 
 'charming', 'clean', 'clear', 'clever', 'cloudy', 'colorful', 'combative', 'cooperative', 'courageous'],
 
     getMessage(){
 
-        let subj = this.subject[Math.floor(Math.random()*2)];
+        let subj = this._subject[Math.floor(Math.random()*2)];
 
         let ver = '';
 
         if(subj === 'You'){
-            ver = this.verb[Math.floor(Math.random()*2)];
+            ver = this._verb[Math.floor(Math.random()*2)];
         }else{
-            ver = this.verb[(Math.floor(Math.random()*2) + 1) % 3];
+            ver = this._verb[(Math.floor(Math.random()*2) + 1) % 3];
         }
         
-        let adj = this.adjective[Math.floor(Math.random()*16)];
+        let adj = this._adjective[Math.floor(Math.random()*16)];
         
-        return `${subj} ${ver} ${adj}`
+        return `${subj} ${ver} ${adj}`;
     }
 };
+
+function message(){
+    document.getElementById("message").innerHTML = str.getMessage();
+}
